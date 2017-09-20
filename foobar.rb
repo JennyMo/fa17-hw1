8e2c82cf9b1f367617f4134b4c37b1ca139946a3
Jenny Mo <jennym826@gmail.com>
2017-09-20 00:48:06 -0700

    Completed Homework 1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,13 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a = a.map {|element| element.to_i + 2}
+    a = a.map {|element| element.to_i if element % 2 == 0}.uniq
+    a = a.map {|element| element.to_i if element.to_i < 10}
+    
+    sum_resultingArray = 0
+    a.each {|element| sum_resultingArray += element.to_i}
+    return sum_resultingArray
   end
 end
 
